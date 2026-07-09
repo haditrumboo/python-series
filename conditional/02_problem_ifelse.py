ba8aeb500dd793c8c7b5239ba624a99cdfc9a690
Hadi <haditrumboo@gmail.com>
2026-07-09 10:38:39 +0530

    feat: add student percentage and pass/fail checker

---
+++ conditional/02_problem_ifelse.py
@@ -0,0 +1,13 @@
+m1 = int(input("enter 1 subject marks: "))
+m2 = int(input("enter 2 subject marks: "))
+m3 = int(input("enter 3 subject marks: "))
+
+total_percentage = (m1 + m2 + m3)/300
+print(total_percentage)
+
+if(total_percentage > 0.40):
+    print("you have passed the exam")
+
+else:
+    print("hard luck")
+    print("WORK HARD")
