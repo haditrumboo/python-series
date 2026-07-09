8a2acf6530ca55b4fad032558f70ba8cbf589ccd
Hadi <haditrumboo@gmail.com>
2026-07-09 10:43:13 +0530

    fix: correct percentage calculation

--- conditional/02_problem_ifelse.py
+++ conditional/02_problem_ifelse.py
@@ -2,10 +2,10 @@ m1 = int(input("enter 1 subject marks: "))
 m2 = int(input("enter 2 subject marks: "))
 m3 = int(input("enter 3 subject marks: "))
 
-total_percentage = (m1 + m2 + m3)/300
+total_percentage = ((m1 + m2 + m3)/300)*100
 print(total_percentage)
 
-if(total_percentage > 0.40):
+if(total_percentage > 40):
     print("you have passed the exam")
 
 else:
