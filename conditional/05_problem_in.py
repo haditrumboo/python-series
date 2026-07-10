bc2c8499960727f38f0b9d23eb03e2d87c3224f0
Hadi <haditrumboo@gmail.com>
2026-07-10 23:00:25 +0530

    Add program to check if a name exists in a list

---
+++ conditional/05_problem_in.py
@@ -0,0 +1,10 @@
+
+enter = input("enter your name: ").strip().lower()
+
+names = ["burhan", "wahid","huxaif","sahil"]
+
+if (enter in names):
+    print("hello",enter )
+
+else:
+    print("sorry, your not in this list.")
