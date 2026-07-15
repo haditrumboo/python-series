1c16e456e9fb6ebf26e3db94f6565bed8edf9128
Hadi <haditrumboo@gmail.com>
2026-07-15 16:00:43 +0530

    feat: add Celsius to Fahrenheit conversion function

---
+++ functions/02_problem_Fahrenheit.py
@@ -0,0 +1,10 @@
+celsius = float(input("celius: "))
+
+def CtoF(c):
+    fahrenheit =  9/5 * c
+    return fahrenheit + 32
+
+
+a = CtoF(celsius)
+
+print(a)
---
+++ functions/function.py
@@ -0,0 +1,9 @@
+def goodday(name, ending):
+    print(f"have a good day {name}")
+    print(ending)
+    return "done"
+
+
+a= goodday("burhan", "thanks")
+print(a)
+
