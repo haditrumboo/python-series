9b01b7962ca7369f16380d6cb996a2b950f604c0
Hadi <haditrumboo@gmail.com>
2026-07-21 22:10:59 +0530

    practice checking file existence and size with os.path

--- os/06_problem.py
+++ os/06_problem.py
@@ -1,17 +1,20 @@
 import os
 
-largest_size = 0
-largest_file = ""
+# largest_size = 0
+# largest_file = ""
 
-for root, folders, files in os.walk("."):
-    for file in files:
-        if file.endswith(".txt"):
-            path = os.path.join(root, file)
-            size = os.path.getsize(path)
+# for root, folders, files in os.walk("."):
+#     for file in files:
+#         if file.endswith(".txt"):
+#             path = os.path.join(root, file)
+#             size = os.path.getsize(path)
 
-            if size > largest_size:
-                largest_size = size
-                largest_file = path
+#             if size > largest_size:
+#                 largest_size = size
+#                 largest_file = path
 
-print(f"Largest file: {largest_file}")
-print(f"Size: {largest_size} bytes")
+# print(f"Largest file: {largest_file}")
+# print(f"Size: {largest_size} bytes")
+
+
+
---
+++ os/07_problem.py
@@ -0,0 +1,5 @@
+
+import os
+
+print(os.path.exists("files/s.txt"))
+print(os.path.getsize("files/s.txt"))
